9908303d51a585f2fb72bd0eedf4641eb3a72390
Marshall Bertschy <basemarsh@gmail.com>
2020-11-06 01:00:52 -0600

    added js variables

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-var btnStart = document.getElementById("buttonStart");
-console.log(btnStart);
-btnStart.addEventListener("click",function(){
-
-title.innerText = "Game Sart: What is your name?"
-});
-
-
-var nextBtn = document.getElementById("nextBtn");
-console.log(nextBtn);
- var title = document.getElementById("headtitle");
- console.log(title)
-
- nextBtn.addEventListener("click",changerTitre)
- function changerTitre(){
-     title.innerText ="je suis Gisele"
- }
+var quizQuestions = document.getElementById("quiz-questions");
+var timer = document.getElementById("timer");
+var btnStart = document.getElementById("btn-start");
+var timecounter = document.getElementById("timecounter");
+var titleitem = document.getElementById("title-item");
+var alert = document.getElementById("alert");
+var questionanswers = document.getElementById("question-answers");
+var myScore = document.getElementById("score");
+var btnScore = document.getElementById("btnScore");
+var nextQuestions
+var info = document.getElementById("info");
+var currentindex = 0;
+var score = 0;
+var count = 90;
+var allScores = [];
+var storedScores = JSON.parse(localStorage.getItem("userData"));
+var questions = [
+    {
+        title: "Commonly used data type Do Not include:---",
+        choices: ["strings", "booleance", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if/else statement is enclosed within:---",
+        choices: ["quotes", "Curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store:---",
+        choices: ["numbers and strings", "others Arrays", "booleances", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within --- when being assigned to variables ",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:---",
+        choices: ["JavaScript", "terminal/bash", "alerts", "console.log"],
+        answer: "console.log"
+    },
+]
